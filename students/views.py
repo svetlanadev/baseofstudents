68c6374fd5aa66046613dacadc8a1a6ac404716e
Svetadev <turistenok1@gmail.com>
2016-05-26 16:20:37 +0300

    settings

--- students/views.py
+++ students/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there world!")
+	return HttpResponse("just test!")
