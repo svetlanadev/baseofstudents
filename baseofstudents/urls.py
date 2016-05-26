838581d905c95643c7d8e994d46ff1e9656f2a3e
Svetadev <turistenok1@gmail.com>
2016-05-26 18:04:48 +0300

    create templates

--- baseofstudents/urls.py
+++ baseofstudents/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-#     url(r'^parametry/', include('parametry.urls')),
-    url(r'^students/', include('students.urls')),   
+    url(r'', include('students.urls')), 
+    url(r'', include('parametry.urls')),
 ]
--- parametry/urls.py
+++ parametry/urls.py
@@ -1,3 +1,6 @@
 from django.conf.urls import patterns, url
 from parametry import views
 
+urlpatterns = [
+	url(r'^$', views.index, name = 'index'),
+]
