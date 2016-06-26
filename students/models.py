946e99452a1b9c22c8a69db4851c3c9ab398895c
Svetadev <turistenok1@gmail.com>
2016-06-26 18:08:19 +0300

    delete some migrations, add button

--- students/models.py
+++ students/models.py
@@ -7,8 +7,8 @@ class Studentsprofile(models.Model):
 	firstname = models.CharField(max_length= 50)
 	lastname = models.CharField(max_length= 50)
 	middlename = models.CharField(max_length= 50)
-	dait_of_birth = models.DateField()
-	# photo = models.ImageField(upload_to='PhofileImage', blank=True)
+	date_of_birth = models.DateField()
+	# photo = models.ImageField(upload_to='ProfileImage', blank=True)
 	students_ticket = models.IntegerField(default=0)
 	group = models.ForeignKey(Groupofstudents)
 
